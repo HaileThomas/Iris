05e49031a14cbeaf51e6a50b40993ac6b88a7138
Thomas Gebremichael Haile <tghaile@passive.mgmt.esrg.stanford.edu>
2026-04-24 10:46:59 -0700

    add packet splitting infrastructure

--- core/src/lcore/monitor.rs
+++ core/src/lcore/monitor.rs
@@ -197,19 +197,21 @@ impl Display {
         let sockets: BTreeSet<_> = ports.keys().map(|id| id.socket_id()).collect();
 
         for socket in sockets {
-            let name = format!("mempool_{}", socket);
-            let cname = CString::new(name.clone()).expect("Invalid CString conversion");
-            let mempool_raw = unsafe { dpdk::rte_mempool_lookup(cname.as_ptr()) };
-            let avail_cnt = unsafe { dpdk::rte_mempool_avail_count(mempool_raw) };
-            let inuse_cnt = unsafe { dpdk::rte_mempool_in_use_count(mempool_raw) };
+            for prefix in ["standard", "split_header", "split_remainder"] {
+                let name = format!("mempool_{}_{}", prefix, socket);
+                let cname = CString::new(name.clone()).expect("Invalid CString conversion");
+                let mempool_raw = unsafe { dpdk::rte_mempool_lookup(cname.as_ptr()) };
+                let avail_cnt = unsafe { dpdk::rte_mempool_avail_count(mempool_raw) };
+                let inuse_cnt = unsafe { dpdk::rte_mempool_in_use_count(mempool_raw) };
 
-            println!(
-                "{} avail: {}, in use: {} ({:.3}%)",
-                name,
-                avail_cnt,
-                inuse_cnt,
-                100.0 * inuse_cnt as f64 / (inuse_cnt + avail_cnt) as f64
-            );
+                println!(
+                    "{} avail: {}, in use: {} ({:.3}%)",
+                    name,
+                    avail_cnt,
+                    inuse_cnt,
+                    100.0 * inuse_cnt as f64 / (inuse_cnt + avail_cnt) as f64
+                );
+            }
         }
     }
 }
@@ -260,7 +262,7 @@ impl Logger {
                 }
                 Err(error) => log::error!("{}", error),
             }
-            let name = format!("mempool_{}", port_id.socket_id());
+            let name = format!("mempool_standard_{}", port_id.socket_id());
             let cname = CString::new(name.clone()).expect("Invalid CString conversion");
             let mempool_raw = unsafe { dpdk::rte_mempool_lookup(cname.as_ptr()) };
             let avail_cnt = unsafe { dpdk::rte_mempool_avail_count(mempool_raw) };
